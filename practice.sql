4d8643471f8fdd5589d9fb793de607a23034ec91
KimDJ <ejuniya@naver.com>
2024-04-30 11:36:38 +0900

    202404301136

--- practice.sql
+++ practice.sql
@@ -1,4 +1,5 @@
 -- 1-1-1
+-- 테이블 생성
 CREATE TABLE product(
 	  Product_Code		varchar2(10)	NOT NULL
 	, Pruduct_Name		varchar2(20)	NOT NULL
@@ -11,7 +12,7 @@ CREATE TABLE product(
 	, Info				varchar2(50)
 	, Detail_Info		varchar2(255)
 );
-
+--pk추가
 ALTER TABLE product
 ADD
 CONSTRAINT product_pk
@@ -21,6 +22,7 @@ PRIMARY key(Product_Code)
 --DROP TABLE users;
 
 -- 1-1-2
+-- 테이블 생성
 CREATE TABLE users(
 	  UserID		varchar2(10)	NOT NULL
 	, Passwd		varchar2(10)	NOT NULL
@@ -32,13 +34,13 @@ CREATE TABLE users(
 	, Buycash		Number(9)		DEFAULT 	0
 	, Timestamps	DATE			DEFAULT 	sysdate
 );
-
+-- pk 추가
 ALTER TABLE users
 ADD
 CONSTRAINT users_pk
 PRIMARY key(UserID)
 ;
-
+-- uk 추가
 ALTER TABLE users
 ADD
 CONSTRAINT users_uk
@@ -46,6 +48,7 @@ unique(Regist_No)
 ;
 
 -- 1-1-3
+-- 테이블 추가
 CREATE TABLE shoppingbag(
 	  Order_No		varchar2(10)	NOT NULL
 	, Order_ID		varchar2(10)	NOT NULL
@@ -53,19 +56,19 @@ CREATE TABLE shoppingbag(
 	, Order_Qty		Number(2)		NOT NULL
 	, Order_Date	DATE			DEFAULT		sysdate
 );
-
+-- pk 추가
 ALTER TABLE shoppingbag
 ADD
 CONSTRAINT shoppingbag_pk
 PRIMARY key(Order_No)
 ;
-
+-- fk 추기
 ALTER TABLE shoppingbag
 ADD CONSTRAINT shoppingbag_users_fk
 FOREIGN key(Order_ID)
 REFERENCES users(UserID)
 ;
-
+-- fk 추가
 ALTER TABLE shoppingbag
 ADD CONSTRAINT shoppingbag_product_fk
 FOREIGN key(Product_Code)
@@ -73,6 +76,7 @@ REFERENCES product(Product_Code)
 ;
 
 -- 1-1-4
+-- 테이블 추가
 CREATE TABLE orderdeal(
 	  Order_No		varchar2(10)	NOT NULL
 	, ORDER_ID		varchar2(10)	NOT NULL
@@ -84,18 +88,20 @@ CREATE TABLE orderdeal(
 	, Mdate			DATE
 	, Gubun			varchar2(10)
 );
-
+-- pk 추가
 ALTER TABLE orderdeal
 ADD CONSTRAINT orderdeal_pk
 PRIMARY key(Order_No)
 ;
 
 -- 1-2
+-- 테이블명 모두 출력
 SELECT *
   FROM USER_TABLES
 ;
 
 -- 1-3
+-- 테이블 구조 출력
 SELECT COLUMN_NAME
 	 , DATA_TYPE
 	 , DATA_LENGTH
@@ -104,21 +110,25 @@ SELECT COLUMN_NAME
 ;
 
 -- 1-4
+-- 컬럼 삭제
 ALTER TABLE product
 DROP COLUMN Detail_info
 ;
 
 -- 1-5
+-- 컬럼 데이터 타입 수정
 ALTER TABLE product
 MODIFY Info varchar2(40)
 ;
 
 -- 1-6
+-- 컬럼 데이터 타입 수정
 ALTER TABLE ORDERDEAL
 MODIFY Gubun varchar2(20)
 ;
 
 -- 1-7
+-- fk 추가
 ALTER TABLE ORDERDEAL 
 ADD CONSTRAINT orderdeal_FK
 FOREIGN key(Product_Code)
@@ -126,6 +136,7 @@ REFERENCES product(Product_Code)
 ;
 
 -- 1-8
+-- 제약 조건 출력
 SELECT uc.CONSTRAINT_NAME 
 	 , uc.CONSTRAINT_TYPE 
   FROM user_constraints uc 
@@ -135,6 +146,7 @@ WHERE uc.TABLE_NAME = 'ORDERDEAL'
 -- 1-9
 
 -- 2-1
+-- 테이블 생성
 CREATE TABLE Freeboard(
 	  B_Id			Number(5)		NOT NULL
 	, B_Name		varchar2(20)	NOT NULL
@@ -148,6 +160,7 @@ CREATE TABLE Freeboard(
 );
 
 -- 2-2
+-- 칼럼 추가
 ALTER TABLE Freeboard
 ADD B_Red 	Number(5)		DEFAULT 0
 ADD B_Step 	Number(5)		DEFAULT 0
@@ -155,21 +168,25 @@ ADD B_Order Number(5)		DEFAULT 0
 ;
 
 -- 2-3
+-- 칼럼 수정
 ALTER TABLE Freeboard
 MODIFY B_Title varchar2(100)
 ;
 
 -- 2-4
+-- 칼럼 수정
 ALTER TABLE Freeboard
 MODIFY B_Pwd NULL
 ;
 
 -- 2-5
+-- 칼럼 삭제
 ALTER TABLE Freeboard
 DROP COLUMN B_Ip
 ;
 
 -- 2-6
+-- 테이블 구조 확인
 SELECT COLUMN_NAME
      , DATA_TYPE
      , DATA_LENGTH
@@ -178,6 +195,7 @@ SELECT COLUMN_NAME
 ;
 
 -- 2-7
+-- 체크 제약조건 추가(영소문자 입력)
 ALTER TABLE users
 ADD CONSTRAINTS UserID_ck
 CHECK (REGEXP_LIKE(UserID, '^[a-z]+$'))
@@ -192,26 +210,31 @@ VALUES ('20park','1234','박연수','810604-1******','010-2345-2345')
 ;
 
 -- 2-8
+-- 칼럼명 변경
 ALTER TABLE Freeboard
 RENAME COLUMN B_Step TO B_Level
 ;
 
 -- 2-9
+-- pk 삭제
 ALTER TABLE orderdeal
 DROP CONSTRAINT orderdeal_pk
 ;
 
 -- 2-10
+-- uk 추가
 ALTER TABLE Freeboard
 ADD CONSTRAINT B_Email_uk
 UNIQUE (B_Email)
 ;
 
 -- 2-11
+-- 테이블 이름 변경
 ALTER TABLE Freeboard
 RENAME TO Free_Board
 ;
 
 -- 2-12
+-- 테이블 삭제
 DROP TABLE SHOPPINGBAG 
 ;
